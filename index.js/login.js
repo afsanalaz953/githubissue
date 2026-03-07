040cdac590e1aa9615a5577c7a0fe166ea01f844
afsanalaz953 <lazafsana7@gmail.com>
2026-03-07 10:20:59 +0600

    Initial commit

---
+++ index.js/login.js
@@ -0,0 +1,18 @@
+console.log("loged in")
+
+document.getElementById("signin-btn").addEventListener("click", function () {
+const userName = document.getElementById("input-text")
+const inputText = userName.value;
+const userPassword = document.getElementById("input-pass")
+const inputPassword = userPassword.value;
+if (inputText == "admin" && inputPassword == "admin123" ) {
+    // alert("login success")
+    window.location.assign("/home.html");
+} else {
+    alert ("login failed")
+    return;
+}
+console.log(inputText, inputPassword)
+
+    console.log("click");
+})
