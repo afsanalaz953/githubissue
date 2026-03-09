d0fead50c438557e47c1f49d642f388d493bfd1a
afsanalaz953 <lazafsana7@gmail.com>
2026-03-09 22:48:45 +0600

    login alert

--- index.js/login.js
+++ index.js/login.js
@@ -6,7 +6,7 @@ const inputText = userName.value;
 const userPassword = document.getElementById("input-pass")
 const inputPassword = userPassword.value;
 if (inputText == "admin" && inputPassword == "admin123" ) {
-    // alert("login success")
+    alert("login success")
     window.location.assign("home.html");
 } else {
     alert ("login failed")
